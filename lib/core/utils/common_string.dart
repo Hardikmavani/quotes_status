c0a92403f93758f25a719b0148b0bfb8ac3b1f2d
Hardikmavani <hardik.mavani300@gmail.com>
2023-10-17 12:10:05 +0530

    commit

--- lib/core/utils/common_string.dart
+++ lib/core/utils/common_string.dart
@@ -18,4 +18,16 @@ class CommonString {
   // for Text
 
   static String vQuotesAboutLife = 'Quotes About Life';
+  static String vQuotesForInspiration = 'Quotes for Inspiration';
+  static String vFullyCustomizableWithBeautifulImages =
+      'Fully customizable with beautiful images ';
+  static String vSetRemiders = 'Set Reminders';
+  static String vGoalLife =
+      'The goal is to help you find the inspiration and motivation you need for living a good and simple life.';
+  static String vQuotesWonderful =
+      'Quotes that inspire you and motivate you for your life and will totally brighten up and make your day wonderful.';
+  static String vQuotesBackground =
+      'Quotes ready with customizing options such as background, text colors, text size, and fonts, or you can choose your photo from your Photo Gallery to be used as background.';
+  static String vSetFamily =
+      'Set personal reminders to nudge yourself to be mindful throughout the day and share the uplifting quote of the day with your friends and family.';
 }
--- lib/modules/splash/view/tutorial_screen.dart
+++ lib/modules/splash/view/tutorial_screen.dart
@@ -1,5 +1,6 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:quotes_status/core/utils/color_constants.dart';
 import 'package:quotes_status/core/utils/common_string.dart';
 import 'package:quotes_status/widgets/custom_image_view.dart';
@@ -12,6 +13,56 @@ class TutorialScreen extends StatefulWidget {
 }
 
 class _TutorialScreenState extends State<TutorialScreen> {
+  List<Widget> title = [
+    Text(
+      CommonString.vQuotesAboutLife,
+      style: GoogleFonts.lindenHill(
+          textStyle: TextStyle(fontSize: 18.5, color: ColorConstant.white)),
+    ),
+    Text(
+      CommonString.vQuotesForInspiration,
+      style: GoogleFonts.lindenHill(
+          textStyle: TextStyle(fontSize: 18.5, color: ColorConstant.white)),
+    ),
+    Text(
+      CommonString.vFullyCustomizableWithBeautifulImages,
+      style: GoogleFonts.lindenHill(
+          textStyle: TextStyle(fontSize: 18.5, color: ColorConstant.white)),
+    ),
+    Text(
+      CommonString.vSetRemiders,
+      style: GoogleFonts.lindenHill(
+          textStyle: TextStyle(fontSize: 18.5, color: ColorConstant.white)),
+    ),
+  ];
+
+  List<Widget> description = [
+    Text(
+      textAlign: TextAlign.center,
+      CommonString.vGoalLife,
+      style: GoogleFonts.itim(
+          textStyle: TextStyle(fontSize: 15, color: ColorConstant.white)),
+    ),
+    Text(
+      textAlign: TextAlign.center,
+      CommonString.vQuotesWonderful,
+      style: GoogleFonts.itim(
+          textStyle: TextStyle(fontSize: 15, color: ColorConstant.white)),
+    ),
+    Text(
+      textAlign: TextAlign.center,
+      CommonString.vQuotesBackground,
+      style: GoogleFonts.itim(
+          textStyle: TextStyle(fontSize: 15, color: ColorConstant.white)),
+    ),
+    Text(
+      textAlign: TextAlign.center,
+      CommonString.vSetFamily,
+      style: GoogleFonts.itim(
+          textStyle: TextStyle(fontSize: 15, color: ColorConstant.white)),
+    ),
+  ];
+
   List<Widget> items = [
     // CustomImageView(
     //     svgPath: CommonString.imgMentionRafiki,
@@ -26,21 +77,17 @@ class _TutorialScreenState extends State<TutorialScreen> {
     //     margin: const EdgeInsets.all(80)),
 
     CustomImageView(
-      imagePath: CommonString.imgMentionRafiki,
-      // margin: const EdgeInsets.all(80)
-    ),
+        imagePath: CommonString.imgMentionRafiki,
+        margin: const EdgeInsets.all(80)),
     CustomImageView(
-      imagePath: CommonString.imgMentionPana,
-      // margin: const EdgeInsets.all(80)
-    ),
+        imagePath: CommonString.imgMentionPana,
+        margin: const EdgeInsets.all(80)),
     CustomImageView(
-      imagePath: CommonString.imgMentionAmico,
-      // margin: const EdgeInsets.all(80)
-    ),
+        imagePath: CommonString.imgMentionAmico,
+        margin: const EdgeInsets.all(80)),
     CustomImageView(
-      imagePath: CommonString.imgMentionCuate,
-      // margin: const EdgeInsets.all(80)
-    ),
+        imagePath: CommonString.imgMentionCuate,
+        margin: const EdgeInsets.all(80)),
   ];
   int currentIndex = 0;
 
@@ -64,27 +111,55 @@ class _TutorialScreenState extends State<TutorialScreen> {
           children: [
             Stack(
               children: [
+                CarouselSlider(
+                  items: title,
+                  options: CarouselOptions(
+                    enlargeCenterPage: true,
+                    autoPlay: true,
+                    onPageChanged: (index, reason) {
+                      setState(() {
+                        currentIndex = index;
+                      });
+                    },
+                  ),
+                ),
                 Container(
-                  margin: EdgeInsets.only(top: dHeight * 0.05),
+                  margin: EdgeInsets.only(top: dHeight * 0.04),
                   height: dHeight * 0.78,
                   decoration: BoxDecoration(
                       image: DecorationImage(
                     image: AssetImage(CommonString.imgBrownBg),
                   )),
-                  child: CarouselSlider(
-                    items: items,
-                    options: CarouselOptions(
-                      height: dHeight * 0.5,
-                      enlargeCenterPage: true,
-                      autoPlay: true,
-                      onPageChanged: (index, reason) {
-                        setState(() {
-                          currentIndex = index;
-                        });
-                      },
-                    ),
+                  child: Column(
+                    children: [
+                      CarouselSlider(
+                        items: items,
+                        options: CarouselOptions(
+                          height: dHeight * 0.5,
+                          enlargeCenterPage: true,
+                          autoPlay: true,
+                          onPageChanged: (index, reason) {
+                            setState(() {
+                              currentIndex = index;
+                            });
+                          },
+                        ),
+                      ),
+                      CarouselSlider(
+                        items: description,
+                        options: CarouselOptions(
+                          enlargeCenterPage: true,
+                          autoPlay: true,
+                          onPageChanged: (index, reason) {
+                            setState(() {
+                              currentIndex = index;
+                            });
+                          },
+                        ),
+                      ),
+                    ],
                   ),
-                )
+                ),
               ],
             ),
             // Row(
